021d93af7437070b617f752ffc42782e832e3ea3
Lei Chen <chenl18@fudan.edu.cn>
2022-10-27 16:10:36 +0800

    Add files via upload

---
+++ model_encoder/bertweet.py
@@ -0,0 +1,31 @@
+﻿## Encoder BERTweet
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+import transformers
+from transformers import AutoModel, AutoTokenizer
+transformers.logging.set_verbosity_error()
+
+class Encoder(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.pretrain_file = args.bert_tweet
+        self.tokenizer = AutoTokenizer.from_pretrained(self.pretrain_file)
+        self.bertweet = AutoModel.from_pretrained(self.pretrain_file)
+        # temp parameters to obtain model device name
+        self.dummy_param = nn.Parameter(torch.empty(0))
+        self.sent_size = 768
+
+    def forward(self, mes):
+        '''Encode textual mesence into embeddings
+        mes(list of str, e.g. ['xxx']): strings of messages
+        '''
+        # extract mesence representation with pre-trained LM
+        inputs = self.tokenizer(
+            mes, padding=True, truncation=True, return_tensors='pt')
+        inputs = {k: v.to(self.dummy_param.device) for k, v in inputs.items()}
+        bert_outputs = self.bertweet(**inputs)
+        mes_embed = bert_outputs.last_hidden_state[:, 0, :]
+        return mes_embed
+        
---
+++ model_integrate/aware.py
@@ -0,0 +1,54 @@
+﻿import torch
+from torch import nn
+from torch.nn import functional as F
+
+class Integrate(nn.Module):
+    def __init__(self, args, input_size=None):
+        super().__init__()
+
+    @staticmethod
+    def _transform_mes_into_cas(message, structure):
+        cids, mids, pids, time = structure
+        # split according to pid=='None'
+        source_index = [i for i in range(len(pids)) if pids[i] == 'None']
+        temp = source_index + [len(pids)]
+        cas_index = [list(range(temp[i], temp[i+1]))
+                    for i in range(len(source_index))]
+        cas_size = [len(index) for index in cas_index]
+        max_cas_size = max(cas_size)
+        cas_batch = [message[index] for index in cas_index]
+        # padding into the same length
+        cas_batch = [F.pad(item, (0, 0, 0, max_cas_size-item.size(0)),
+                        'constant', 0) for item in cas_batch]
+        # stack to form uniform tensor
+        cas_all = torch.stack(tuple(cas_batch), dim=0)
+        cas_mask = torch.Tensor([[1]*item+[0]*(max_cas_size-item)
+                                for item in cas_size]).long().to(message.device)
+        return cas_all, cas_mask, source_index
+
+    def aware_pooling(self, x, x_mask, weights):
+        """
+        x: (bsz, seq_len, input_size)
+        x_mask: (bsz, seq_len)
+        weights: (bsz, seq_len, 1)
+        => s: (bsz, input_size)-pooling output, u: (bsz, seq_len)-pooling weights
+        """
+        mm_tensor = torch.Tensor([0,1,1,1]).to(weights.device)
+        weight_score = torch.matmul(weights, mm_tensor)  # (bsz, seq_len)
+        u = weight_score.masked_fill_(x_mask==0, -1e9)        
+        u = F.softmax(u, dim=-1)
+        u_ = u.unsqueeze(-1).expand(-1, -1, x.size(-1))
+        s = torch.sum(x*u_, dim=1) 
+        return s, u
+
+    def forward(self, mes_update, yv, structure, *args, **kargs):
+        '''ingerate mesences to form cas-level representation
+        mes_update(torch.Tensor): updated message representation, shape: (batch_mes, hidden)
+        '''
+        yt_pred = kargs.pop('yt_pred', None)
+        cas_all, cas_mask, source_index = self._transform_mes_into_cas(
+            mes_update, structure)  # (batch_cas, max_cas_size, hidden), (batch_cas, max_cas_size)
+        yt_pred, cas_mask, source_index = self._transform_mes_into_cas(yt_pred, structure)
+        cas, weights = self.aware_pooling(cas_all, cas_mask, yt_pred) # (batch_cas, hidden)
+        yv_cas = yv[source_index]
+        return cas, yv_cas
